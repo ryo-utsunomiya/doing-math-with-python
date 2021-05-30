0fe19992157a224e8218245f08a8091353cc1904
Ryo Utsunomiya <millionsnowliving@gmail.com>
2021-05-31 01:15:04 +0900

    add factors

---
+++ factors.py
@@ -0,0 +1,20 @@
+def factors(b):
+    """
+    Find the factors of an integer
+    """
+    ret = []
+    for i in range(1, b + 1):
+        if b % i == 0:
+            ret.append(i)
+    return ret
+
+
+if __name__ == '__main__':
+    b = float(input('Your Number Please: '))
+
+    if b <= 0 or (not b.is_integer()):
+        print('Please enter a positive integer')
+        exit(1)
+
+    for i in factors(int(b)):
+        print(i)
